aaafaeda84bbbe265539ef3f7d59a9bde48002c2
Dileep Kishore <k.dileep1994@gmail.com>
2025-07-28 20:17:28 -0400

    feat(nvim): add options and better escape plugin

--- private_dot_config/nvim/lua/plugins/main.lua
+++ private_dot_config/nvim/lua/plugins/main.lua
@@ -20,6 +20,21 @@ return {
     },
   },
 
+  {
+    "max397574/better-escape.nvim",
+    config = function()
+      require("better_escape").setup({
+        default_mappings = false,
+        mappings = {
+          c = { j = { k = "<Esc>" } },
+          i = { j = { k = "<Esc>" } },
+          s = { j = { k = "<Esc>" } },
+          v = { j = { k = "<Esc>" } },
+        },
+      })
+    end,
+  },
+
   -- change trouble config
   {
     "folke/trouble.nvim",
