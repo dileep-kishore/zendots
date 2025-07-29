e474cb0aa939f2ecb2e5fdcdd3edde4e3413b380
Dileep Kishore <k.dileep1994@gmail.com>
2025-07-28 20:20:48 -0400

    feat(nvim): add keymaps and autocmds

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -1,5 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
 require("config.options")
 require("config.keymaps")
+require("config.lazy")
+require("config.keymaps-plugins")
+require("config.keymaps-snacks")
 require("config.autocmds")
--- private_dot_config/nvim/lua/config/autocmds.lua
+++ private_dot_config/nvim/lua/config/autocmds.lua
@@ -1,8 +1,83 @@
--- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
---
--- Add any additional autocmds here
--- with `vim.api.nvim_create_autocmd`
---
--- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
--- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
+  callback = function()
+    vim.cmd("set formatoptions-=cro")
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = {
+    "netrw",
+    "Jaq",
+    "qf",
+    "git",
+    "help",
+    "man",
+    "lspinfo",
+    "oil",
+    "alpha",
+    "spectre_panel",
+    "lir",
+    "DressingSelect",
+    "",
+  },
+  callback = function()
+    vim.cmd([[  
+      nnoremap <silent> <buffer> q :close<CR>  
+      set nobuflisted  
+    ]])
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "CmdWinEnter" }, {
+  callback = function()
+    vim.cmd("quit")
+  end,
+})
+
+local auto_resize_group = vim.api.nvim_create_augroup("_auto_resize", { clear = true })
+vim.api.nvim_create_autocmd({ "VimResized" }, {
+  group = auto_resize_group,
+  pattern = "*",
+  command = [[
+          let _auto_resize_current_tab = tabpagenr()
+          tabdo wincmd =
+          execute 'tabnext' _auto_resize_current_tab
+        ]],
+})
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = {
+    "netrw",
+    "Jaq",
+    "qf",
+    "git",
+    "help",
+    "man",
+    "lspinfo",
+    "oil",
+    "spectre_panel",
+    "lir",
+    "DressingSelect",
+    "neogitstatus",
+    "alpha",
+    "notify",
+    "dashboard",
+    "Trouble",
+    "toggleterm",
+    "",
+  },
+  callback = function()
+    vim.b.miniindentscope_disable = true
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "WinEnter", "WinNew" }, {
+  callback = function()
+    local win = vim.api.nvim_get_current_win()
+    local config = vim.api.nvim_win_get_config(win)
+    -- Only adjust floating windows (those with a non-empty 'relative' field)
+    if config.relative ~= "" then
+      vim.api.nvim_set_option_value("winblend", 0, { win = win })
+    end
+  end,
+})
