c7a2e44cd9ba5cd6156ae65ded527b53511774b3
Dileep Kishore <k.dileep1994@gmail.com>
2025-07-29 13:28:32 -0400

    feat(yazi): add plugin script and pkgs for yazi

---
+++ download_yazi_plugins.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+ya pkg add yazi-rs/plugins:git
+ya pkg add yazi-rs/plugins:piper
+ya pkg add yazi-rs/plugins:jump-to-char
+ya pkg add AnirudhG07/rich-preview
+ya pkg add ndtoan96/ouch
