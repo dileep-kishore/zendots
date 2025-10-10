bb5a3337ffba008b62ff89f9b1dccf873aa839f7
Dileep Kishore <k.dileep1994@gmail.com>
2025-10-09 22:07:57 -0400

    feat(nvim): update dashboard with startup + polish aesthetics

--- private_dot_config/nvim/lua/ui/snacks-dashboard.lua
+++ private_dot_config/nvim/lua/ui/snacks-dashboard.lua
@@ -1,4 +1,4 @@
-local snacks = require 'snacks'
+local snacks = require("snacks")
 local M = {}
 
 M.dashboard = {
@@ -6,42 +6,43 @@ M.dashboard = {
   preset = {
     keys = {
       {
-        icon = ' ',
-        key = 'f',
-        desc = 'Find File',
-        action = ':lua Snacks.picker.files()',
+        icon = " ",
+        key = "f",
+        desc = "Find File",
+        action = ":lua Snacks.picker.files()",
       },
       {
-        icon = ' ',
-        key = 'n',
-        desc = 'New File',
-        action = ':ene | startinsert',
+        icon = " ",
+        key = "n",
+        desc = "New File",
+        action = ":ene | startinsert",
       },
       {
-        icon = ' ',
-        key = 'g',
-        desc = 'Find Text',
-        action = ':lua Snacks.picker.grep()',
+        icon = " ",
+        key = "g",
+        desc = "Find Text",
+        action = ":lua Snacks.picker.grep()",
       },
       {
-        icon = ' ',
-        key = 'p',
-        desc = 'Find project',
-        action = ':lua Snacks.picker.projects()',
+        icon = " ",
+        key = "p",
+        desc = "Find project",
+        action = ":lua Snacks.picker.projects()",
       },
       {
-        icon = ' ',
-        key = 't',
-        desc = 'Find todos',
-        action = ':lua Snacks.picker.todo_comments()',
+        icon = " ",
+        key = "t",
+        desc = "Find todos",
+        action = ":lua Snacks.picker.todo_comments()",
       },
       {
-        icon = ' ',
-        key = 'r',
-        desc = 'Restore Session',
-        action = ':PossessionLoadCwd',
+        icon = "󰁯 ",
+        key = "r",
+        desc = "Restore Session",
+        action = ":PossessionLoadCwd",
       },
-      { icon = ' ', key = 'q', desc = 'Quit', action = ':qa' },
+      { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+      { icon = " ", key = "q", desc = "Quit", action = ":qa" },
     },
     header = [[
                                                                              /\_/\                             
@@ -60,51 +61,34 @@ M.dashboard = {
   formats = {
     key = function(item)
       return {
-        { '[', hl = 'special' },
-        { item.key, hl = 'key' },
-        { ']', hl = 'special' },
+        { "[", hl = "special" },
+        { item.key, hl = "key" },
+        { "]", hl = "special" },
       }
     end,
   },
   sections = {
     {
-      section = 'header',
+      section = "header",
       indent = -5,
     },
     {
       -- icon = ' ',
       -- title = 'Builtin Actions',
-      section = 'keys',
+      section = "keys",
       gap = 1,
       padding = 1,
     },
+    { section = "startup", padding = 1 },
     {
-      text = '󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘 󰄛 󰄛 󰄛 󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘',
-      padding = 1,
-    },
-    {
-      -- icon = ' ',
-      title = 'Recent Files',
-      section = 'recent_files',
+      text = "󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘 󰄛 󰄛 󰄛 󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘󰇘",
       padding = 1,
     },
     {
       -- icon = ' ',
-      title = 'Projects',
-      section = 'projects',
+      title = "Projects",
+      section = "projects",
       padding = 1,
-    },
-    {
-      -- icon = ' ',
-      title = 'Git Status',
-      section = 'terminal',
-      enabled = function()
-        return snacks.git.get_root() ~= nil
-      end,
-      cmd = 'git status --short --branch --renames',
-      height = 5,
-      padding = 1,
-      ttl = 5 * 60,
     },
   },
 }
