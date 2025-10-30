a9f65007f30468287f2eca469686f03768eb57f3
Dileep Kishore <k.dileep1994@gmail.com>
2025-10-30 00:00:56 -0400

    feat(pkgs): refactor pkgs

--- pkgs/misc_setup.sh
+++ pkgs/misc_setup.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
 # Rebuild bat cache
-/usr/bin/bat cache --build
+if [ -f /opt/homebrew/bin/bat ]; then
+    BAT_PATH="/opt/homebrew/bin/bat"
+else
+    BAT_PATH="/usr/bin/bat"
+fi
+BAT_PATH cache --build
 
 # Clone tpm
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+if [ ! -d ~/.tmux/plugins/tpm ]; then
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
--- pkgs/pipx_packages.sh
+++ pkgs/pipx_packages.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-pipx install shell-gpt
 pipx install copier
